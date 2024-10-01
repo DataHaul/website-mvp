f12555c8febaa85621c55110b4a307e08d529041
Christopher Wiafe Debrah <53785246+askdebb@users.noreply.github.com>
2024-10-01 20:08:48 +0000

    update some features

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Navbar from "./components/NavBar";
 import HeroSection from "./components/Hero";
-import { Container } from '@mui/material';
+import { Container } from "@mui/material";
 import FeaturesSection from "./components/About";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-<>
-    <Navbar/>
-    <HeroSection/>
-    <Features/>
-    <FeaturesSection/>
-    <Footer/>
+    <>
+      <Navbar />
+      <HeroSection />
+      <Features />
+      <FeaturesSection/>
+      <Footer />
     </>
   );
 }
